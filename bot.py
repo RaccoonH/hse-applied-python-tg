4c9213cfb63e2e721347a0115d952e449c48443f
Andrey Tudunov <a.tudunov@life-stream.tv>
2026-01-17 00:47:14 +0700

    Add webhook option

--- bot.py
+++ bot.py
@@ -1,21 +1,36 @@
+import asyncio
 from aiogram import Bot, Dispatcher
-from config import TOKEN
+from config import TOKEN, WEBHOOK
 from handlers import set_profile, main_commands
 from middleware import CommandMiddleware
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 
 
+async def main():
+    if WEBHOOK:
+        bot = Bot(token=TOKEN)
+        dp = Dispatcher()
+        dp.message.middleware(CommandMiddleware())
+        dp.include_routers(main_commands.router, set_profile.router)
+
+        app = web.Application()
+        webhook_requests_handler = SimpleRequestHandler(dp, bot)
+        webhook_requests_handler.register(app, path="/webhook")
+        setup_application(app, dp, bot=bot)
+
+        await bot.delete_webhook(drop_pending_updates=True)
+        await bot.set_webhook(url=WEBHOOK)
+
+        print("Бот запущен!")
+        web.run_app(app, host="0.0.0.0", port=10000)
+    else:
+        bot = Bot(token=TOKEN)
+        dp = Dispatcher()
+        dp.message.middleware(CommandMiddleware())
+        dp.include_routers(main_commands.router, set_profile.router)
+        await dp.start_polling(bot)
+
+
 if __name__ == "__main__":
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher()
-    dp.message.middleware(CommandMiddleware())
-    dp.include_routers(main_commands.router, set_profile.router)
-
-    app = web.Application()
-    webhook_requests_handler = SimpleRequestHandler(dp, bot)
-    webhook_requests_handler.register(app, path="/webhook")
-    setup_application(app, dp, bot=bot)
-
-    print("Бот запущен!")
-    web.run_app(app, host="0.0.0.0", port=10000)
+    asyncio.run(main())
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 WEATHER_TOKEN = os.getenv("WEATHER_TOKEN")
+WEBHOOK = os.getenv("WEBHOOK")
 
 if not TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
