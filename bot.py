7cedbc8467e0a00d80f7fe31ddd01eee200f27ae
Andrey Tudunov <a.tudunov@life-stream.tv>
2026-01-17 00:25:57 +0700

    Add webhook

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
-import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import set_profile, main_commands
 from middleware import CommandMiddleware
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-dp.message.middleware(CommandMiddleware())
-dp.include_routers(main_commands.router, set_profile.router)
-
-
-async def main():
-    print("Бот запущен!")
-    await dp.start_polling(bot)
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.message.middleware(CommandMiddleware())
+    dp.include_routers(main_commands.router, set_profile.router)
+
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(dp, bot)
+    webhook_requests_handler.register(app, path="/webhook")
+    setup_application(app, dp, bot=bot)
+
+    print("Бот запущен!")
+    web.run_app(app, host="0.0.0.0", port=10000)
