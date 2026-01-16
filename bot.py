38f44f14f1b5ee00bd36dc06b0c8f6dce4c567dd
Andrey Tudunov <a.tudunov@life-stream.tv>
2026-01-17 01:05:14 +0700

    Fix

--- bot.py
+++ bot.py
@@ -7,7 +7,20 @@ from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_applicati
 from aiohttp import web
 
 
+async def set_webhook(bot):
+    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_webhook(url=WEBHOOK)
+
+
 async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+    dp.message.middleware(CommandMiddleware())
+    dp.include_routers(main_commands.router, set_profile.router)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
     if WEBHOOK:
         bot = Bot(token=TOKEN)
         dp = Dispatcher()
@@ -18,19 +31,8 @@ async def main():
         webhook_requests_handler = SimpleRequestHandler(dp, bot)
         webhook_requests_handler.register(app, path="/webhook")
         setup_application(app, dp, bot=bot)
-
-        await bot.delete_webhook(drop_pending_updates=True)
-        await bot.set_webhook(url=WEBHOOK)
-
+        asyncio.run(set_webhook(bot))
         print("Бот запущен!")
         web.run_app(app, host="0.0.0.0", port=10000)
     else:
-        bot = Bot(token=TOKEN)
-        dp = Dispatcher()
-        dp.message.middleware(CommandMiddleware())
-        dp.include_routers(main_commands.router, set_profile.router)
-        await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+        asyncio.run(main())
